318ceabc34edfb17f2d8709bcb2cc4bcce5ddd44
louis <surwing912@gmail.com>
2025-01-04 13:29:39 +0800

    [planner] small modification

--- Learn_Python/Exercise 3.py
+++ Learn_Python/Exercise 3.py
@@ -47,23 +47,23 @@
 #     print(item)
 
 # 创建字典存储车票信息,使用车次做key,使用其他信息做value
-dict_ticket={
-    'G1569':['北京南-天津南', '18:06', '18:39', '00:33'],
-    'G1567':['北京南-天津南', '18:15', '18:49', '00:34'],
-    'G8917':['北京南-天津西', '18:20', '19:19', '00:59'],
-    'G203':['北京南-天津南', '18:35', '19:09', '00:34']
-}
-print('车次            出发站-到达站           出发时间        到达时间        历时时长')
-for key in dict_ticket.keys():
-    print(key, end='\t\t')
-    for item in dict_ticket.get(key):
-        print(item, end='\t\t')
-    print()
-train_no=input('请输入要购买的车次:')
-info=dict_ticket.get(train_no, '车次不存在')
-if info!='车次不存在':
-    person=input('请输入乘车人,如果是多位乘车人使用逗号分隔:')
-    s=info[0]+' '+info[1]+'开'
-    print('您已购买了'+train_no+' '+s+',请'+person+'尽快换取纸质车票')
-else:
-    print('对不起,选择的车次可能不存在')
+# dict_ticket={
+#     'G1569':['北京南-天津南', '18:06', '18:39', '00:33'],
+#     'G1567':['北京南-天津南', '18:15', '18:49', '00:34'],
+#     'G8917':['北京南-天津西', '18:20', '19:19', '00:59'],
+#     'G203':['北京南-天津南', '18:35', '19:09', '00:34']
+# }
+# print('车次            出发站-到达站           出发时间        到达时间        历时时长')
+# for key in dict_ticket.keys():
+#     print(key, end='\t\t')
+#     for item in dict_ticket.get(key):
+#         print(item, end='\t\t')
+#     print()
+# train_no=input('请输入要购买的车次:')
+# info=dict_ticket.get(train_no, '车次不存在')
+# if info!='车次不存在':
+#     person=input('请输入乘车人,如果是多位乘车人使用逗号分隔:')
+#     s=info[0]+' '+info[1]+'开'
+#     print('您已购买了'+train_no+' '+s+',请'+person+'尽快换取纸质车票')
+# else:
+#     print('对不起,选择的车次可能不存在')
